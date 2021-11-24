cd63d5c0082dd8ab72e34e1a96e94da582e8778d
reanaldy <wiriatamareanaldy@gmail.com>
2021-11-24 14:36:08 +0700

    menambahkan folder dan file pada includes dan pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -13,4 +13,22 @@
         <h1>Setup Laravel Bootstrap Successfully<i class="fas fa-ad"></i></h1>
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
+</html> -->
+
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('pages.header')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
 </html>
